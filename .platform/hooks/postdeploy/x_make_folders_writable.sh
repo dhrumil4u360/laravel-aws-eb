4bb72a1e44216ea8938d225f20c3cd7b457c86c8
dhrumil4u360 <dhrumil4u360@gmail.com>
2020-12-04 16:23:08 +0530

    Add permissions for webapp user
    We can restrict permissions to 775 and change ownership to webapp. When deployment is happening and there is some log entries in laravel.log, that file is owned by root by default. That means when actual application log is coming from laravel (which is under webapp user), permission to access it is denied if it's 775.
    
    This solution should restrict permission reasonably with laravel still able to write.

--- .platform/hooks/postdeploy/x_make_folders_writable.sh
+++ .platform/hooks/postdeploy/x_make_folders_writable.sh
@@ -6,5 +6,8 @@
 # to some folders that should be writable, such as the storage/
 # or bootstrap/cache/, for example.
 
-sudo chmod -R 777 storage/
-sudo chmod -R 777 bootstrap/cache/
+sudo chmod -R 775 storage/
+sudo chmod -R 775 bootstrap/cache/
+sudo chmod +x artisan
+sudo chown -R webapp:webapp storage/
+sudo chown -R webapp:webapp bootstrap/cache/
